443977a0d56aa0962a5b7f5722f7576448a5f938
Lucas Manhães <ramosmanhaes@outlook.com>
2025-02-12 21:30:22 -0300

    Adicionando React Router, componente cadastro e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
 import Footer from './components/footer/footer'
 import Home from './components/home/home'
+import Cadastro from './components/cadastro/cadastro'
+import Login from './components/login/login'
 
 export default function App() {
 
   return (  
     <>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar/>
-        <Home/>
-        <Footer/>
-      </div>
+      <BrowserRouter>
+            <div className='h-screen flex flex-col justify-between'>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Login/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/cadastro" element={<Cadastro/>} />
+                <Route path="/home" element={<Home/>} />
+              </Routes>
+              <Footer />
+            </div>
+        </BrowserRouter>
     </>
   )
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-2'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        Sair
+                        <p className="mx-6">Postagens</p>
+                        <p className="mx-6">Temas</p>
+                        <p className="mx-6">Cadastrar Temas</p>
+                        <p className="mx-6">Perfil</p>
+                        <p className="mx-6">Sair</p>
                     </div>
                 </div>
             </div>
